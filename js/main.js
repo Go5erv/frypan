e1e3fc30cb48c50fa1e01927ec9230f8daff6b1f
Go5erv <145844105+Go5erv@users.noreply.github.com>
2024-02-18 06:25:14 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// Loading
+$(window).load(function(){
+  //Loading Screen 
+  $(".loading, .loading .sk-chase").fadeOut(2000);
+}); 
+
 // ---------Responsive-navbar-active-animation-----------
 function test(){
 	var tabsNewAnim = $('#navbarSupportedContent');
@@ -38,6 +44,56 @@ $(".navbar-toggler").click(function(){
 	$(".navbar-collapse").slideToggle(300);
 	setTimeout(function(){ test(); });
 });
+// Slider
+(function($) {
+  $.fn.menumaker = function(options) {
+      var cssmenu = $(this),
+          settings = $.extend({
+              format: "dropdown",
+              sticky: false
+          }, options);
+      return this.each(function() {
+          $(this).find(".button").on('click', function() {
+              $(this).toggleClass('menu-opened');
+              var mainmenu = $(this).next('ul');
+              if (mainmenu.hasClass('open')) {
+                  mainmenu.slideToggle().removeClass('open');
+              } else {
+                  mainmenu.slideToggle().addClass('open');
+                  if (settings.format === "dropdown") {
+                      mainmenu.find('ul').show();
+                  }
+              }
+          });
+          cssmenu.find('li ul').parent().addClass('has-sub');
+          multiTg = function() {
+              cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+              cssmenu.find('.submenu-button').on('click', function() {
+                  $(this).toggleClass('submenu-opened');
+                  if ($(this).siblings('ul').hasClass('open')) {
+                      $(this).siblings('ul').removeClass('open').slideToggle();
+                  } else {
+                      $(this).siblings('ul').addClass('open').slideToggle();
+                  }
+              });
+          };
+          if (settings.format === 'multitoggle') multiTg();
+          else cssmenu.addClass('dropdown');
+          if (settings.sticky === true) cssmenu.css('position', 'fixed');
+          resizeFix = function() {
+              var mediasize = 1000;
+              if ($(window).width() > mediasize) {
+                  cssmenu.find('ul').show();
+              }
+              if ($(window).width() <= mediasize) {
+                  cssmenu.find('ul').hide().removeClass('open');
+              }
+          };
+          resizeFix();
+          return $(window).on('resize', resizeFix);
+      });
+  };
+})(jQuery);
 
 // --------------add active class-on another-page move----------
 jQuery(document).ready(function($){
@@ -76,18 +132,161 @@ function myFunction() {
 
 
 // Scroll Top
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-}
+const button = document.querySelector('.scroll-top');
 
-// Loading
-$(window).load(function(){
-    //Loading Screen 
-    $(".loading, .loading .sk-chase").fadeOut(2000);
+const displayButton = () => {
+  window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+  
+    if (window.scrollY > 100) {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  });
+};
+
+const scrollToTop = () => {
+  button.addEventListener("click", () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    }); 
+    console.log(event);
+  });
+};
+
+displayButton();
+scrollToTop();
+
+// Swipper Banner
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 15000,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
+
+// vars
+'use strict'
+var	testim = document.getElementById("testim"),
+		testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+    testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+    testimLeftArrow = document.getElementById("left-arrow"),
+    testimRightArrow = document.getElementById("right-arrow"),
+    testimSpeed = 4500,
+    currentSlide = 0,
+    currentActive = 0,
+    testimTimer,
+		touchStartPos,
+		touchEndPos,
+		touchPosDiff,
+		ignoreTouch = 30;
+;
+
+window.onload = function() {
+
+    // Testim Script
+    function playSlide(slide) {
+        for (var k = 0; k < testimDots.length; k++) {
+            testimContent[k].classList.remove("active");
+            testimContent[k].classList.remove("inactive");
+            testimDots[k].classList.remove("active");
+        }
+
+        if (slide < 0) {
+            slide = currentSlide = testimContent.length-1;
+        }
+
+        if (slide > testimContent.length - 1) {
+            slide = currentSlide = 0;
+        }
+
+        if (currentActive != currentSlide) {
+            testimContent[currentActive].classList.add("inactive");            
+        }
+        testimContent[slide].classList.add("active");
+        testimDots[slide].classList.add("active");
+
+        currentActive = currentSlide;
+    
+        clearTimeout(testimTimer);
+        testimTimer = setTimeout(function() {
+            playSlide(currentSlide += 1);
+        }, testimSpeed)
+    }
+
+    testimLeftArrow.addEventListener("click", function() {
+        playSlide(currentSlide -= 1);
+    })
+
+    testimRightArrow.addEventListener("click", function() {
+        playSlide(currentSlide += 1);
+    })    
+
+    for (var l = 0; l < testimDots.length; l++) {
+        testimDots[l].addEventListener("click", function() {
+            playSlide(currentSlide = testimDots.indexOf(this));
+        })
+    }
+
+    playSlide(currentSlide);
+
+    // keyboard shortcuts
+    document.addEventListener("keyup", function(e) {
+        switch (e.keyCode) {
+            case 37:
+                testimLeftArrow.click();
+                break;
+                
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            default:
+                break;
+        }
+    })
+		
+		testim.addEventListener("touchstart", function(e) {
+				touchStartPos = e.changedTouches[0].clientX;
+		})
+	
+		testim.addEventListener("touchend", function(e) {
+				touchEndPos = e.changedTouches[0].clientX;
+			
+				touchPosDiff = touchStartPos - touchEndPos;
+			
+				console.log(touchPosDiff);
+				console.log(touchStartPos);	
+				console.log(touchEndPos);	
+
+			
+				if (touchPosDiff > 0 + ignoreTouch) {
+						testimLeftArrow.click();
+				} else if (touchPosDiff < 0 - ignoreTouch) {
+						testimRightArrow.click();
+				} else {
+					return;
+				}
+			
+		})
+}
 
 // swiper
 var swiper = new Swiper('.swiper-initialize', {
@@ -112,3 +311,18 @@ var swiper = new Swiper('.swiper-initialize', {
       prevEl: '.swiper-button-prev'
     }
   });
+
+// Get the current year and time
+function getCurrentDateTime() {
+  var now = new Date();
+  var year = now.getFullYear();
+
+  return year
+}
+
+// animation wow js
+new WOW().init();
+
+// Update the content of the h2 element
+var copyrightElement = document.getElementById('copyright');
+copyrightElement.innerHTML += ' ' + getCurrentDateTime();
